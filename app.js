dd3a494c9753fa9f7ece61547f896ddc715a390f
Dan Vera <dan@yovu.co>
2014-12-01 23:50:26 -0500

    more

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var camera = new RaspiCam({
   output: "./codes/image_%06d.jpg",
   encoding: "jpg",
   timelapse: 2000,
+  timeout: 12000,
   nopreview: true
 });
 
@@ -13,8 +14,26 @@ camera.on("start", function( err, timestamp ){
   console.log("timelapse started at " + timestamp);
 });
 
-camera.on("read", function( err, timestamp, filename ){
-  console.log("timelapse image captured with filename: " + filename);
+camera.on("read", function( err, timestamp, imagename ){
+  console.log("timelapse image captured with filename: " + imagename);
+
+  // process this baby
+  var Canvas = require('canvas'), Image = Canvas.Image, qrcode = require('jsqrcode')(Canvas);
+
+  var filename = __dirname + '/codes/' + imagename;
+
+  var image = new Image();
+  image.onload = function(){
+    var result;
+    try {
+      result = qrcode.decode(image);
+      console.log('result of qr code: ' + result);
+    } catch(e) {
+      console.log('unable to read qr code');
+    }
+  }
+  image.src = filename;
+
 });
 
 camera.on("exit", function( timestamp ){
@@ -26,3 +45,7 @@ camera.on("stop", function( err, timestamp ){
 });
 
 camera.start();
+
+setTimeout(function(){
+  camera.stop();
+}, 9000);
