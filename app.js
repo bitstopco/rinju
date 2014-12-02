4b2942056e8db50aa32b1592e1b5e83378b0e224
Dan Vera <dan@yovu.co>
2014-12-01 22:27:46 -0500

    test

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 var fs = require('fs');
-var Camera = require("camerapi");
+var RaspiCam = require("raspicam");
 
-var cam = new Camera();
+var camera = new RaspiCam({
+  mode: "photo",
+  output: "qrcode.png",
+  encoding: "png",
+  timeout: 1
+});
 
-cam.baseFolder(__dirname + '/codes');
 
-cam.prepare(
-  {
-    "timeout" : 200, 
-    "width" : 2592,
-    "height" : 1944,
-    "quality" : 100
-  }
-).takePicture("qrcode.jpg",callback);
+  camera.on("started", function( err, timestamp ){
+    console.log("photo started at " + timestamp );
+  });
 
-function callback(file,error){
+  camera.on("read", function( err, timestamp, filename ){
+    console.log("photo image captured with filename: " + filename );
 
-  console.log('Cool');
+    // process this baby
+    var Canvas = require('canvas'), Image = Canvas.Image, qrcode = require('jsqrcode')(Canvas);
 
-}
+    var filename = __dirname + '/qrcode.png';
+
+    var image = new Image();
+    image.onload = function(){
+      var result;
+      try {
+        result = qrcode.decode(image);
+        console.log('result of qr code: ' + result);
+      } catch(e) {
+        console.log('unable to read qr code');
+        runIt('2');
+      }
+    }
+    image.src = filename;
+
+  });
+
+  camera.on("exit", function( timestamp ){
+    console.log("photo child process has exited at " + timestamp );
+  });
+
+  camera.start();
+
