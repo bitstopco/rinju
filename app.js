bee979b556f5b36b95b78ff3118eb34738e21a00
Dan Vera <dan@yovu.co>
2014-12-01 23:45:09 -0500

    time

--- app.js
+++ app.js
@@ -2,42 +2,27 @@ var fs = require('fs');
 var RaspiCam = require("raspicam");
 
 var camera = new RaspiCam({
-  mode: "photo",
-  output: "qrcode.png",
-  encoding: "png",
-  timeout: 2
+  mode: "timelapse",
+  output: "./codes/image_%06d.jpg",
+  encoding: "jpg",
+  timelapse: 2000,
+  nopreview: true
 });
 
+camera.on("start", function( err, timestamp ){
+  console.log("timelapse started at " + timestamp);
+});
 
-  camera.on("started", function( err, timestamp ){
-    console.log("photo started at " + timestamp );
-  });
+camera.on("read", function( err, timestamp, filename ){
+  console.log("timelapse image captured with filename: " + filename);
+});
 
-  camera.on("read", function( err, timestamp, filename ){
-    console.log("photo image captured with filename: " + filename );
+camera.on("exit", function( timestamp ){
+  console.log("timelapse child process has exited");
+});
 
-    // process this baby
-    var Canvas = require('canvas'), Image = Canvas.Image, qrcode = require('jsqrcode')(Canvas);
+camera.on("stop", function( err, timestamp ){
+  console.log("timelapse child process has been stopped at " + timestamp);
+});
 
-    var filename = __dirname + '/qrcode.png';
-
-    var image = new Image();
-    image.onload = function(){
-      var result;
-      try {
-        result = qrcode.decode(image);
-        console.log('result of qr code: ' + result);
-      } catch(e) {
-        console.log('unable to read qr code');
-      }
-    }
-    image.src = filename;
-
-  });
-
-  camera.on("exit", function( timestamp ){
-    console.log("photo child process has exited at " + timestamp );
-  });
-
-  camera.start();
-
+camera.start();
