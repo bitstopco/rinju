08441eb8982e65f1f9e0706e970e9152e60351f2
Dan Vera <dan@yovu.co>
2014-12-02 20:59:59 -0500

    scan

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var Camelittle = require('camelittle');
+var clInstance = new Camelittle({});
+var Canvas = require('canvas'), Image = Canvas.Image, qrcode = require('jsqrcode')(Canvas);
 
 function runIt(seconds) {
   setTimeout(runFaucet, seconds * 1000);
@@ -6,6 +9,27 @@ function runIt(seconds) {
 
 function runFaucet()  {
   console.log('Hi')
+
+  clInstance.grab(function(err, image){
+    console.log('Taken');
+
+    fs.writeFileSync('codes/code.jpg', image, 'binary');
+
+    var filename = __dirname + '/codes/code.jpg'
+
+    var image = new Image()
+    image.onload = function(){
+    var result;
+      try{
+        result = qrcode.decode(image)
+        console.log(result);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    image.src = filename
+
+  });
 
   runIt('3');
 }
